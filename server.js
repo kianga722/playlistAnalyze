240492d762aba37478b6eb2961e6240f98e45e30
kianga722 <kianga722@gmail.com>
2020-05-24 16:30:49 -0400

    Prep for heroku deploy

--- server.js
+++ server.js
@@ -133,6 +133,14 @@ app.post('/api/', async (req, res) => {
   })
 });
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 // start the server
 app.listen(port, () => {
